c0a7895970de17e6634f47dea23489f90730f0fd
Adam Schmalhofer <code@adaschma.name>
2025-09-05 12:51:25 +0200

    Fixup

--- src/Ganeti/Query/Export.hs
+++ src/Ganeti/Query/Export.hs
@@ -72,7 +72,7 @@ exportFields =
   , (FieldDefinition "export_du" "ExpDUsage" QFTText "Use size on disk for export",
      FieldRuntime (\_ n -> rsNormal $ nodeName n), QffHostname)
   , (FieldDefinition "export_dtotal" "ExpDTotal" QFTText "Total storage space on /var/lib/ganiti/exports",
-     FieldRuntime (\_ n -> "hallo"), QffHostname)
+     FieldRuntime (\_ n -> rsNormal $ "hallo"), QffHostname)
   ]
 
 -- | The node fields map.
