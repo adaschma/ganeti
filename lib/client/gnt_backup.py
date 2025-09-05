cd84d994b06ad9ea52665796c5f10ef731899a2b
Adam Schmalhofer <code@adaschma.name>
2025-09-05 09:37:58 +0200

    Try export_du being a copy of export

--- lib/client/gnt_backup.py
+++ lib/client/gnt_backup.py
@@ -42,7 +42,7 @@ from ganeti import errors
 from ganeti import qlang
 
 
-_LIST_DEF_FIELDS = ["node", "export", "export_du"]
+_LIST_DEF_FIELDS = ["node", "export", "export_du", "export_df"]
 
 
 def PrintExportList(opts, args):
