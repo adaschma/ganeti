8521ef666597d0edb836ff1a933b13df75b71906
Adam Schmalhofer <code@adaschma.name>
2025-09-09 16:59:35 +0200

    Fixup

--- src/Ganeti/Query/Node.hs
+++ src/Ganeti/Query/Node.hs
@@ -165,7 +165,7 @@ nodeLiveFieldExtract "export_dfree" res =
 nodeLiveFieldExtract _ _ = J.JSNull
 
 getStorageInfoName :: Maybe StorageInfo -> J.JSValue
-getStorageInfoName = getAttrFromStorageInfo (\n -> Just $ J.toJSString "welt")
+getStorageInfoName = getAttrFromStorageInfo (\n -> Just $ J.toJSString storageInfoName)
 
 -- | Helper for extracting field from RPC result.
 nodeLiveRpcCall :: FieldName -> Runtime -> Node -> ResultEntry
